4f80b8b1b64ae2023f05db33a6b3d509ed7b0c09
Jake Stoeffler <stoefflerj@gmail.com>
2013-09-14 21:40:11 -0500

    STuff

--- twit1.js
+++ twit1.js
@@ -4,28 +4,33 @@ if (Meteor.isClient) {
   console.log("Hey");
   
   Template.twitboard.twits = function() {
-    return Twits.find();
+    return Twits.find({}, {sort: {timeStamp: -1}});
   };
-  
-  $("button").on("click", function() {
-    console.log("YO");
-    Twits.insert(
-      {
-        text: $("#postText").val(),
-        author: "Bob",
-        time: new Date().getTime().toString()
-      });
-    return false;
-  });
   
   Template.post.events({
     'click #postSubmit': function() {
       console.log("YO");
+      if (!Meteor.user()) {
+        alert("You must log in to create a post!");
+        return;
+      }
+      var email = Meteor.user().emails[0].address;
+      var handle = email.substring(0, email.indexOf("@"));
+    
+      if (email === "admin@admin.admin" && $("#postText").val() === "cl34r") {
+        var allTwits = Twits.find().fetch();
+        for (var i = 0; i < allTwits.length; i++) {
+          Twits.remove(allTwits[i]._id);
+        }
+        return;
+      }
+    
       Twits.insert(
         {
           text: $("#postText").val(),
-          author: "Bob",
-          time: new Date().getTime().toString()
+          author: handle,
+          time: getDateString(),
+          timeStamp: new Date()
         });
       return false;
     }
@@ -36,4 +41,17 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function getDateString() {
+  var a = new Date();
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+   var year = a.getFullYear();
+   var month = months[a.getMonth()];
+   var date = a.getDate();
+   var hour = a.getHours();
+   var min = a.getMinutes();
+   var sec = a.getSeconds();
+   var time = date+' '+month+' '+year+' at '+hour+':'+min+':'+sec ;
+   return time;
 }
